2edb9f5b56118684752835dfef0f907d658d1259
inaba <kuchibuefuite@icloud.com>
2020-07-12 09:09:22 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,28 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  def index
+    @topic = Topic.find(params[:id])
+    @comments = @topic.comments
+    @comment = @topic.contents.build
+
+  end
 
   def create
     topic = Tocic.find(params[:topic_id])
-    @comments = Comment.new
-    @comments.user_id = current_user.id
-    @comments.topic_id = params[:topic_id]
+    @comment = Comment.new
+    @comment.user_id = current_user.id
+    @comment.topic_id = params[:topic_id]
 
-    if @comments.save
+    if @comment.save
       redirect_to topics_path, success: 'コメントしました'
     else
       redirect_to topics_path, danger: 'コメントできませんでした'
     end
   end
 
-  private
 
+  private
   def comment_params
     params.require(:comment).permit(:content)
   end
-
-
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,8 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all.includes(:favorite_users)
+    @comment = Comment.new
+
   end
 
   def new
